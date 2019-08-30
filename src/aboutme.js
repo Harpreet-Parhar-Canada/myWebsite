f5f73699af0c0e8903b547385bc3283e7d6c3481
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2019-08-30 15:23:17 -0600

    fix

--- src/aboutme.js
+++ src/aboutme.js
@@ -37,13 +37,13 @@ class About extends React.Component {
                                     
                                      
                                      
-                                </div>
                                 <div className=" animated fadeIn delay-5s duration-10s"> 
                                     <h1>Interest</h1>
                                     <p className="aboutp"><iframe src="https://giphy.com/embed/xT9DPzhNGA8MKjxwFG" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen title="Reading"></iframe> <iframe src="https://giphy.com/embed/lqSDx8SI1916ysr4eq" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen title="Music"></iframe><iframe src="https://giphy.com/embed/b5Hcaz7EPz26I" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen title="Cooking"></iframe></p>
                                     <p> Reading  &nbsp;&nbsp;&nbsp;&nbsp;    Listening to music  &nbsp;&nbsp;&nbsp;&nbsp;  Cooking </p>
                                      
-                                     
+                                    </div>
+                             
                                 </div>
                             </Cell>
                         </Grid>
