f242c3f0006c897ce4eb27ae94f1138a0c073f44
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2019-08-31 08:28:33 -0600

    harpreet

--- src/aboutme.js
+++ src/aboutme.js
@@ -37,8 +37,7 @@ class About extends React.Component {
                                     <h1>Interest</h1>
                                     <p className="aboutp">
 <iframe src="https://giphy.com/embed/xT9DPzhNGA8MKjxwFG" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen title="Reading"></iframe> <iframe src="https://giphy.com/embed/lqSDx8SI1916ysr4eq" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen title="Music"></iframe><iframe src="https://giphy.com/embed/b5Hcaz7EPz26I" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen title="Cooking"></iframe>
-<iframe src="https://giphy.com/embed/12W5Sg2koWYnwA" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-</p></div>       
+<iframe src="https://giphy.com/embed/12W5Sg2koWYnwA" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p></div>       
                                        </Grid>
          
                                      
