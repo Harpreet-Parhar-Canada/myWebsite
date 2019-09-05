689d20c98cde73c251ad8fff3ba059d573aafd86
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2019-09-05 12:12:19 -0600

    pract axios

--- src/contact.js
+++ src/contact.js
@@ -18,7 +18,7 @@ class Contact extends Component {
           
   }
   componentDidMount() {
-    axios('https://my-resume-image-data.herokuapp.com/')
+    axios.get('http://myresumedata.us-east-1.elasticbeanstalk.com/')
     .then(res =>{
       this.setState({imgData:res.data});
        return (res.data);})
