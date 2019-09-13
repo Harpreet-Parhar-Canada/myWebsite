13d7188ec8a28b83961ccf259ecb320b60f635a0
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2019-09-13 11:44:12 -0600

    changing

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,BrowserRouter} from 'react-router-dom';
 import Main from './main';
 import About from './aboutme';
 import Resume from './resume';
@@ -7,13 +7,14 @@ import Contact from './contact';
 
 
 const Home = () =>(
+        <BrowserRouter>
        <Switch >
         <Route exact path="/" component={Main} ></Route>
         <Route path="/resume" component={Resume}></Route>
         <Route path="/contact" component={Contact}></Route>
         <Route path="/aboutme" component={About}></Route> 
         </Switch>
-        
+        </BrowserRouter>
         
     
    
